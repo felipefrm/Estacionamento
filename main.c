94aac74da274c7f799ccbdd19866b33fb893b673
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-03-12 17:43:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,14 @@
 #include "configEstacionamento.h"
 #include "manobras.h"
 
-int main(void){
-                                  // formação do estacionamento
-  int **mapa = criaMapa();        // atraves de uma matriz
-  Auto *veiculo = leituraConfigInicial();
+
+int main(int argc, char *argv[]){
+
+  Arquivos* arq = argumentosEntrada(argc, argv);
+  int **mapa = criaMapa();
+  Auto *veiculo = leituraConfigInicial(arq->veiculos);
   configInicialMapa(qtdVeiculos, veiculo, mapa);
-  leituraExecucaoManobra(veiculo, mapa);
+  leituraExecucaoManobra(veiculo, mapa, arq->manobras);
+  free(arq);
   imprimeMapa(mapa);
 }
