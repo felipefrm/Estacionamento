25aecb43ba2b5a6eaa04b3e7c9894a8d346793bf
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-03-12 17:44:18 -0300

    Update manobras.h

--- manobras.h
+++ manobras.h
@@ -2,7 +2,8 @@
 #ifndef MANOBRAS
 #define MANOBRAS
 
-void contaTempo(double *utime, double *stime);
+struct timeval contaTempoRelogio();
+void contaTempoProcessador(double *utime, double *stime);
 void apagaPosAnterior(Auto veiculo, int **mapa);
 int movimentaVeiculo(Auto *veiculo, int **mapa, Movimento manobra, int id);
 int realizaManobra(int qtdVeiculos, Auto *veiculo, Movimento manobra, int **mapa);
