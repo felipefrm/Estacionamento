67ae2079e557af39117549562c141759eb579ae4
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-03-12 17:44:58 -0300

    Update entradaSaida.h

--- entradaSaida.h
+++ entradaSaida.h
@@ -1,6 +1,11 @@
 #ifndef ENTRADASAIDA
 #define ENTRADASAIDA
 
+struct arquivos {
+  FILE* manobras;
+  FILE* veiculos;
+}; typedef struct arquivos Arquivos;
+
 struct automovel {
   char id, tamanho, direcao;    // estrutura que especifíca cada veiculo
   int x, y;
@@ -13,12 +18,14 @@ struct movimento {
 
 int qtdVeiculos;
 
+Arquivos* argumentosEntrada(int argc, char* argv[]);
 int convertCharToInt(char num);
 int calculaQtdVeiculos(FILE* arq);
 void setVeiculo(int qtdVeiculos, FILE *arq, Auto *veiculo);
-Auto* leituraConfigInicial();
+Auto* leituraConfigInicial(FILE* arq_veiculos);
 int verificaSinal(char sinal, int num);
 Movimento leituraManobra(FILE *arq_manobra);
-int leituraExecucaoManobra(Auto *veiculo, int **mapa);
+int leituraExecucaoManobra(Auto *veiculo, int **mapa, FILE* arq_manobras);
+void imprimeMapa(int **mapa);
 
 #endif
